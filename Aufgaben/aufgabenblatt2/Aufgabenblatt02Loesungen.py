7e44f4969637ccdf3cf44dd5a5ffabb0615e182f
jesse <jessestrunskyy@gmail.com>
2025-10-22 16:37:21 +0200

    Aufgabe 1 und 2 readme

--- Aufgaben/aufgabenblatt2/Aufgabenblatt02Loesungen.py
+++ Aufgaben/aufgabenblatt2/Aufgabenblatt02Loesungen.py
@@ -1,11 +1,11 @@
 def ist_prim(n):
-    if n < 2:
-        for i in range(2, int(n-1)):
-            if n % i == 0:
+
+    if(n > 1):
+        for i in range(2, n-1):
+            if(n % i == 0):
                 return False
+
+        return True
     else:
         return False
-
-
-
-ist_prim(25)
+print(ist_prim(29))
