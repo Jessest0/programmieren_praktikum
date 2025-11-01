8fd3cf0932abbea5dd39cdda97e8d4a0f726a79e
jesse <jessestrunskyy@gmail.com>
2025-11-01 13:34:04 +0100

    Aufgabe 3

--- Aufgaben/aufgabenblatt04/Aufgabenblatt04_Loesungen.py
+++ Aufgaben/aufgabenblatt04/Aufgabenblatt04_Loesungen.py
@@ -1,1 +1,14 @@
-print("Hello World")
+class Person:
+    def __init__(self, fName, lName, birth):
+        def __str__(self):
+            self.fName = fName
+            self.lName = lName
+            self.birth = birth
+
+        def __str__(self, fName, lName, birth):
+            self.fName = str(fName)
+            self.lName = str(lName)
+            self.birth = str(birth)
+
+x = Person("Jesse", "Strunsky", 2005)
+print(x.__str__())
