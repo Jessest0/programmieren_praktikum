0f23a3a6693af7b1ab3ba411f7d59ea0ac9f6b3f
jesse <jessestrunskyy@gmail.com>
2025-11-05 16:01:26 +0100

    Aufgabenblatt05

--- Aufgaben/aufgabenblatt05/Aufgabenblatt05_Loesungen.py
+++ Aufgaben/aufgabenblatt05/Aufgabenblatt05_Loesungen.py
@@ -1,1 +1,35 @@
-print("Hello World")
+import datetime
+
+class Person:
+    def __init__(self, fName, lName, birth):
+        self.fName = fName
+        self.lName = lName
+        self.birth = birth
+
+    def __str__(self):
+        return f'{self.fName} {self.lName} {self.birth}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def alter(self):
+        heute = datetime.date.today()
+        jahre = heute.year - self.birth.year
+
+        if (heute.month, heute.day) < (self.birth.month, self.birth.day):
+            jahre-= 1
+
+        return jahre
+
+    def naechster_geburtstag(self):
+        heute = datetime.date.today()
+        jahre = heute.year
+
+        if (heute.month, heute.day) > (self.birth.month, self.birth.day):
+            jahre += 1
+
+        return datetime.date(jahre, self.birth.month, self.birth.day)
+
+class Personendatenbank:
+    def __init__(self, personendaten):
+        self.personendaten = personendaten
